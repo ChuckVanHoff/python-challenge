273991e6c405556892ab1620a8372bb6454982ce
Flying Dutchman <brian.vanhoff@gmail.com>
2018-12-15 21:56:23 -0500

    Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,81 @@
 
+
+import os
+import csv
+import sys
+
+# Declare the file path name
+budget_data_csv = "Resources/budget_data.csv"
+
+# Text file to be written and exported
+analysis = "financial_analysis.txt"
+
+# Open and read csv
+with open(budget_data_csv, 'r', newline="") as budgetfile:
+    csvreader = csv.reader(budgetfile, delimiter=",")
+
+    # Read the header row first
+    csv_header = next(csvreader)
+    
+    print(type(csv_header))
+
+    # Creates a list of strings, each one being a separate row of the CSV file, and 
+    # the variables to store the valuse
+    lines = budgetfile.readlines()
+    monthly_pnl = []
+    month = []
+    
+    # A variable to keep net profit/losses sum
+    net_pnl = 0
+
+    # Some variables to hold and store monthly_change values
+    change = float
+    monthly_change = []
+
+    # Split each line at the comma and add the split line to separated_lines
+    for row in lines:
+        row = row.split(",")
+        
+        # Add values to the monthly_pnl and month lists
+        monthly_pnl.append(row[1])
+        month.append(row[0])
+        
+        # Sum pofit and loss values
+        net_pnl += float(row[1])
+
+    # Add values to monthly_change list
+    for x in range(len(monthly_pnl) - 1):
+        change = float(monthly_pnl[x+1]) - float(monthly_pnl[x])
+        monthly_change.append(change)
+
+    # Calculate the average change between months
+    average_change = sum(monthly_change) / len(monthly_change)
+
+    # Variables for the max and min change
+    max_change = float
+    min_change = float
+    max_change_month_index = int
+    min_change_month_index = int
+
+    # Assign values
+    max_change = max(monthly_change) 
+    min_change = min(monthly_change)
+    max_change_month_index = monthly_change.index(max(monthly_change))
+    min_change_month_index = monthly_change.index(min(monthly_change))
+
+    # Print the financial analysis to terminal and financial_analysis.txt
+    print("Financial Analysis\n" + "_"*30)
+    print("Total Months: " + str(len(lines)))
+    print("Total:  " + str(net_pnl))
+    print("Average Change = " + str(average_change))
+    print(f'Greatest Increase in Profits: {month[max_change_month_index]} ({max_change})')
+    print(f'Greatest Decrease in Profits: {month[min_change_month_index]} ({min_change})')
+
+    f = open(analysis,"a+")
+    f.write("Financial Analysis\n" + "_"*30 + "\n")
+    f.write("Total Months: " + str(len(lines)) + "\n")
+    f.write("Total:  " + str(net_pnl) + "\n")
+    f.write("Average Change = " + str(average_change) + "\n")
+    f.write(f'Greatest Increase in Profits: {month[max_change_month_index]} ({max_change})\n')
+    f.write(f'Greatest Decrease in Profits: {month[min_change_month_index]} ({min_change})\n')
+    f.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,86 @@
+# A program to analyzes election votes and calculate each of the following:
 
+#   * The total number of votes cast
+
+#   * A complete list of candidates who received votes
+
+#   * The percentage of votes each candidate won
+
+#   * The total number of votes each candidate won
+
+#   * The winner of the election based on popular vote.
+
+#   * Print the analysis to the terminal and a text file
+
+import os
+import csv
+
+# Name the file paths and names for read and write files
+poll_data = os.path.join("..", "Resources", "election_data.csv")
+election_results = "election_results.txt"
+
+# Open the read file
+with open(poll_data, 'r', newline="") as results:
+    csvreader = csv.reader(results, delimiter=",")
+
+    # Step past the header
+    next(csvreader)
+
+    # Variables to hole vote total and candidate names and vote counts
+    total = 0
+    candidates = {}
+    
+    # Step though the remaining rows
+    for row in csvreader:
+        
+        # If this row has the first instance of a candidate's name, put it in the dictionary
+        if not row[2] in candidates:
+        
+            # This adds an entry to the dictionary and sets its value to 1
+            candidates[row[2]] = 1
+        
+        # If this row has a repeated entry of the candidate's name add it to the dicitonary
+        if row[2] in candidates:
+            candidates[row[2]] += 1
+        # Add one to the total vote count
+        total += 1
+
+    # Variables for the winning vote count and winner name
+    winning_count = 0
+    winner = str
+
+    # Step through candidate dictionary
+    for candidate in candidates:
+
+        # Find the winner and the vote count
+        if candidates[candidate] > winning_count:
+            winning_count = candidates[candidate] 
+            winner = candidate
+    
+    # Print election resuts to terminal
+    print("Election Results\n" + \
+    "-------------------------" + "\n" + \
+    f"Total Votes: {total}" + "\n" + \
+    "-------------------------" + "\n")
+
+    for candidate in candidates:
+        print(f"{candidate}: {candidates[candidate]/total:.1%} ({candidates[candidate]})")
+        
+    print("-------------------------" + "\n" + \
+    "Winner: " + winner + "\n" + \
+    "-------------------------")
+
+    # Print to text file
+    f = open(election_results,"a+")
+    f.write("Election Results\n" + \
+    "-------------------------" + "\n" + \
+    f"Total Votes: {total}" + "\n" + \
+    "-------------------------" + "\n")
+
+    for candidate in candidates:
+        f.write(f"{candidate}: {candidates[candidate]/total:.1%} ({candidates[candidate]})")
+
+    f.write("-------------------------" + "\n" + \
+    "Winner: " + winner + "\n" + \
+    "-------------------------")
+    f.close()
